ff41ddb62807947db94d002a75dd33088b831e58
Existing_Human <43016462+Maxwell12345@users.noreply.github.com>
2019-06-25 18:53:58 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,8 +9,27 @@
 #include "Globals.h"
 #include "Net.cpp"
 
+//IGNOR EVERYTHING IN HERE IT IS JUST A TESTING BLOCK, LIKE PROOF OF CONCEPT
+
+void NodeLayers::setInitWeights(){
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 3; j ++) {
+            this->initWeightTempHold.push_back(j + i + 1);
+        }
+        this->layerInitWeights.push_back(this->initWeightTempHold);
+    }
+}
+
 int main(int argc, const char * argv[]) {
+    NodeLayers a;
     
-    std::cout << "Hello, World!\n";
+    a.setInitWeights();
+    
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 3; j ++) {
+            std::cout << a.getLayerInitWeight(i, j) << std::endl;
+        }
+    }
+    
     return 0;
 }
