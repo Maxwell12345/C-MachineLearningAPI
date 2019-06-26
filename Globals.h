70b80fc86929a3686272c82f81aac84f1bc45455
Existing_Human <43016462+Maxwell12345@users.noreply.github.com>
2019-06-26 10:54:13 -0400

    Update Globals.h

--- Globals.h
+++ Globals.h
@@ -25,4 +25,10 @@ float setRand(void){
 }
 #define random setRand()
 
+//Note: Plz tell me if u change these, cus they will destroy the whole net if messed with
+#define Input_Neuron_Layer_Size 4
+#define Hidden1_Neuron_Layer_Size 3
+#define Hidden2_Neuron_Layer_Size 3
+#define Output_Neuron_Layer_Size 2
+
 #endif /* Globals_h */
