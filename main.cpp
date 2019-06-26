77b30252753ee31171f9d39126122d92f350949a
Existing_Human <43016462+Maxwell12345@users.noreply.github.com>
2019-06-25 22:06:58 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,20 +5,10 @@
 //  Created by maxwell on 6/25/19.
 //  Copyright © 2019 organized-organization. All rights reserved.
 //
-
 #include "Globals.h"
-#include "Net.cpp"
+#include "Net.hpp"
 
 //IGNOR EVERYTHING IN HERE IT IS JUST A TESTING BLOCK, LIKE PROOF OF CONCEPT
-
-void NodeLayers::setInitWeights(){
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 3; j ++) {
-            this->initWeightTempHold.push_back(j + i + 1);
-        }
-        this->layerInitWeights.push_back(this->initWeightTempHold);
-    }
-}
 
 int main(int argc, const char * argv[]) {
     NodeLayers a;
@@ -29,6 +19,9 @@ int main(int argc, const char * argv[]) {
         for (int j = 0; j < 3; j ++) {
             std::cout << a.getLayerInitWeight(i, j) << std::endl;
         }
+    }
+    for (int i = 0; i < 5; i ++) {
+        std::cout << setRand() << std::endl;
     }
     
     return 0;
